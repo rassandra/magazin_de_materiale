3b4748a240d48b3fb30b41d997395559fb478500
Sirbu Andreea <rassandra21@gmail.com>
2018-03-04 18:06:55 +0200

    merge DB, avem primul template, schita

--- admin/index.php
+++ admin/index.php
@@ -1,3 +1,24 @@
 <?php
+// setup environment
+ini_set('display_errors', '1');
+error_reporting(-1);
+date_default_timezone_set('UTC');
 
-echo "test";
+// include DB passwords
+require_once("pass.php");
+
+
+// connect to DB
+$conn = new PDO('mysql:host=localhost;dbname=andra_magazin_de_materiale', DB_USERNAME, DB_PASSWORD);
+$conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+//$conn->setAttribute( PDO::ATTR_EMULATE_PREPARES, FALSE );
+
+// test insert
+/*
+$conn->prepare("INSERT INTO `adminUsers` SET `username`=?, `password`=?")->execute([
+    'test',
+    md5('test'),
+]);
+*/
+
+require("display-auth.php");
